2abaddc7bb2ec63ada78a38407f7820e5b15c8bd
Thomas Rademaker <thomas@radmakr.com>
2024-07-29 09:16:11 -0400

    beef up download manager logic

--- Effem/Libs/DownloadManager.swift
+++ Effem/Libs/DownloadManager.swift
@@ -15,22 +15,49 @@ import PodcastIndexKit
 class DownloadManager {
     var hasDownloadsInProgress = false
     private let modelContainer: ModelContainer
-
-    
-    // TODO: figure out how to track downloads in this array and have it update the hasDownloadsInProgress bool
-    private var downloads: [Episode] = []
+    private var inProgressDownloads = [Episode : Task<(), Never>]() {
+        didSet { hasDownloadsInProgress != inProgressDownloads.isEmpty }
+    }
     
     init(modelContainer: ModelContainer) {
         self.modelContainer = modelContainer
     }
-
-    func downloadEpisode(_ episode: Episode) async throws {
-//        defer { downloads.rem }
-//        downloads.append(episode)
-        guard let guid = episode.guid, let feedID = episode.feedId else { return }
-        guard let episode = try await EpisodesService().episodes(byGUID: guid, feedid: "\(feedID)").episode else { return }
-        let data = try await DownloadService().downloadEpisode(from: episode.enclosureUrl)
-        try await DownloadPersistor(modelContainer: modelContainer).saveEpisode(episode, with: data)
+    
+    func downloadEpisode(_ episode: Episode) {
+        guard inProgressDownloads[episode] == nil else { return }
+        
+        // REMEMBER - Tasks eat errors
+        // Intentionally ignoring errors here. If it fails we'll cache it next time
+        let task = Task.detached { [weak self] in
+            guard let self, let guid = episode.guid, let feedID = episode.feedId else { return }
+            
+            do {
+                try Task.checkCancellation()
+                guard let episode = try await EpisodesService().episodes(byGUID: guid, feedid: "\(feedID)").episode else { return }
+                let data = try await DownloadService().downloadEpisode(from: episode.enclosureUrl)
+                try await DownloadPersistor(modelContainer: self.modelContainer).saveEpisode(episode, with: data)
+            } catch {
+                await removeDownloadRequestValue(forKey: episode)
+                return
+            }
+        }
+        
+        inProgressDownloads[episode] = task
+    }
+    
+    // MARK: in progress downloads
+    private func removeDownloadRequestValue(forKey key: Episode) {
+        inProgressDownloads.removeValue(forKey: key)
+    }
+    
+    func cancelPrefetch(for episode: Episode) {
+        guard let task = inProgressDownloads[episode] else { return }
+        task.cancel()
+        inProgressDownloads.removeValue(forKey: episode)
+    }
+    
+    func clearCache() {
+        inProgressDownloads.values.forEach { $0.cancel() }
     }
 }
 
